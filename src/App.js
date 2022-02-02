00d3b6faa8867de1fb05ce1e02e65d54a4aa54b7
Sirius Sufiew <siriusproger@gmail.com>
2022-02-02 22:09:32 +0500

    new deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Salom Alif Tech
+          Alif Tech
         </a>
       </header>
     </div>
